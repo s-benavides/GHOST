186ac97e3be7b9c3a1cb7889ad903f3fa019c346
Santiago Benavides <santib@mit.edu>
2019-07-02 11:03:42 -0400

    added kcut def

--- 3D/src/include/rmhdb_cfl.f90
+++ 3D/src/include/rmhdb_cfl.f90
@@ -2,6 +2,7 @@
         CALL maxabs(ax,ay,az,rmq,1)  ! max j = k B
         tmp = sqrt((omegax)**2+(omegay)**2+(omegaz)**2)
         tmq = sqrt((bx0)**2+(by0)**2+(bz0)**2)
+        kcut = real(nx,kind=GP)/3.0_GP    !1/dx        
 
         dt = cfl/max(tmp,rmq,tmp,kcut*tmq,nu*kcut**2,mu*kcut**2)
       CALL MPI_BCAST(dt,1,GC_REAL,0,MPI_COMM_WORLD,ierr)
