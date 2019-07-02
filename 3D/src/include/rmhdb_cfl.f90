c357995f9f84134b9a95375f26a64a3eacfd854c
Santiago Benavides <santib@mit.edu>
2019-07-02 00:03:37 -0400

    Instead of defining a subroutine, which is impossible to generalize for all cases, we will simply calculate dt with this file.

--- 3D/src/include/rmhdb_cfl.f90
+++ 3D/src/include/rmhdb_cfl.f90
@@ -1,2 +1,9 @@
-        CALL CFL_condition(cfl,vx,vy,vz,omegax,omegay,omegaz, &
-                           ax,ay,az,bx0,by0,bz0,nu,mu,dt)
+        CALL maxabs(vx,vy,vz,rmp,0)  ! max vort = k U
+        CALL maxabs(ax,ay,az,rmq,1)  ! max j = k B
+        tmp = sqrt((omegax)**2+(omegay)**2+(omegaz)**2)
+        tmq = sqrt((bx0)**2+(by0)**2+(bz0)**2)
+
+        dt = cfl/max(tmp,rmq,tmp,kcut*tmq,nu*kcut**2,mu*kcut**2)
+      CALL MPI_BCAST(dt,1,GC_REAL,0,MPI_COMM_WORLD,ierr)
+
+
