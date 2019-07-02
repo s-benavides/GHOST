157df31fbce00f2023fef8c94918f44e7b1079f2
Santiago Benavides <santib@mit.edu>
2019-07-02 10:58:20 -0400

    Calculated time (into 'dump' variable) so that we can input time into mhdcheck, which it now accepts

--- 3D/src/include/rmhdb_global.f90
+++ 3D/src/include/rmhdb_global.f90
@@ -1,28 +1,19 @@
 ! Global quantities computed in MHD runs
 #ifdef CFL_
-        ta = time
+        dump = time
 #else
-        ta = t
+        dump = (t-1)*dt
 #endif
-            CALL mhdcheck(vx,vy,vz,ax,ay,az,ta,dt,1,1,0)
+            CALL mhdcheck(vx,vy,vz,ax,ay,az,dump,dt,1,1,0)
             CALL cross(vx,vy,vz,fx,fy,fz,eps,1)
             CALL cross(ax,ay,az,mx,my,mz,epm,0)
             CALL maxabs(vx,vy,vz,rmp,0)
             CALL maxabs(ax,ay,az,rmq,1)
             IF (myrank.eq.0) THEN
                OPEN(1,file='injection.txt',position='append')
-#ifdef CFL_
-               WRITE(1,FMT='(E13.6,E22.14,E22.14)') time,eps,epm
-#else
-               WRITE(1,FMT='(E13.6,E22.14,E22.14)') (t-1)*dt,eps,epm
-#endif
+               WRITE(1,FMT='(E13.6,E22.14,E22.14)') dump,eps,epm
                CLOSE(1)
                OPEN(1,file='maximum.txt',position='append')
-#ifdef CFL)
-               WRITE(1,FMT='(E13.6,E13.6,E13.6)') time,rmp,rmq
-#else
-               WRITE(1,FMT='(E13.6,E13.6,E13.6)') (t-1)*dt,rmp,rmq
-
-#endif
+               WRITE(1,FMT='(E13.6,E13.6,E13.6)') dump,rmp,rmq
                CLOSE(1)
             ENDIF
