9263a016ff416c9d51c4456512c134de0279e400
Santiago Benavides <santib@mit.edu>
2019-07-03 15:52:28 -0400

    Modified nu*kcut**2 to nu*kcut**(2*hek) and similarly for the magnetic component

--- 3D/src/include/rmhdb_cfl.f90
+++ 3D/src/include/rmhdb_cfl.f90
@@ -4,7 +4,8 @@
         tmq = sqrt((bx0)**2+(by0)**2+(bz0)**2)
         kcut = real(nx,kind=GP)/3.0_GP    !1/dx        
 
-        dt = cfl/max(tmp,rmq,tmp,kcut*tmq,nu*kcut**2,mu*kcut**2)
-      CALL MPI_BCAST(dt,1,GC_REAL,0,MPI_COMM_WORLD,ierr)
+        dt = cfl/max(tmp,rmq,tmp,kcut*tmq,nu*kcut**(2*hek),&
+                   mu*kcut**(2*hem),hnu,hmu)
+        CALL MPI_BCAST(dt,1,GC_REAL,0,MPI_COMM_WORLD,ierr)
 
 
