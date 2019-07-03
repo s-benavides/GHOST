ba452289936d3a9d137539361aa66b5006342c80
Santiago Benavides <santib@mit.edu>
2019-07-03 17:57:46 -0400

    Made mhd_global compatible with cfl (but MHD solver doesn't have CFL, it's just that one can now use mhd solver). Also added hyper/hypo diffusion stuff to mhdcheck

--- 3D/src/include/mhd_global.f90
+++ 3D/src/include/mhd_global.f90
@@ -1,15 +1,15 @@
 ! Global quantities computed in MHD runs
 
-            CALL mhdcheck(vx,vy,vz,ax,ay,az,dump,dt,1,1,0)
-            CALL cross(vx,vy,vz,fx,fy,fz,eps,1)
-            CALL cross(ax,ay,az,mx,my,mz,epm,0)
+            CALL mhdcheck(vx,vy,vz,ax,ay,az,hek,hok,hem,hom,dump,dt,1,1,0)
+            CALL cross(vx,vy,vz,fx,fy,fz,eps,1) ! v.f
+            CALL cross(ax,ay,az,mx,my,mz,epm,0) ! b.curl(fb)
             CALL maxabs(vx,vy,vz,rmp,0)
             CALL maxabs(ax,ay,az,rmq,1)
             IF (myrank.eq.0) THEN
                OPEN(1,file='injection.txt',position='append')
-               WRITE(1,FMT='(E13.6,E22.14,E22.14)') (t-1)*dt,eps,epm
+               WRITE(1,FMT='(E13.6,E22.14,E22.14)') dump,eps,epm
                CLOSE(1)
                OPEN(1,file='maximum.txt',position='append')
-               WRITE(1,FMT='(E13.6,E13.6,E13.6)') (t-1)*dt,rmp,rmq
+               WRITE(1,FMT='(E13.6,E13.6,E13.6)') dump,rmp,rmq
                CLOSE(1)
             ENDIF
