54dd87a5bfa3fb8004d453c073172abc99b93774
Santiago Benavides <santib@mit.edu>
2019-07-05 17:34:26 -0400

    replaced laplak3_v2 with diss, which calculates -(nu*k^(2*hek)+hnu*k^(2*hok))a

--- 3D/src/include/hd_rkstep2.f90
+++ 3D/src/include/hd_rkstep2.f90
@@ -4,9 +4,9 @@
          CALL nonlhd3(C4,C5,C6,C7,1)      ! projects to incompressible
          CALL nonlhd3(C4,C5,C6,C8,2)
          CALL nonlhd3(C4,C5,C6,C4,3)
-         CALL laplak3_v2(vx,vx,hek,hok)   ! vx = -(k^(2*hek)+k^(-2*hok))*vx
-         CALL laplak3_v2(vy,vy,hek,hok)
-         CALL laplak3_v2(vz,vz,hek,hok)
+         CALL diss(vx,vx,hek,hok,nu,hnu)   ! vx = -(k^(2*hek)+k^(-2*hok))*vx
+         CALL diss(vy,vy,hek,hok,nu,hnu)
+         CALL diss(vz,vz,hek,hok,nu,hnu)
          IF ((trans.eq.1).and.(times.eq.0).and.(bench.eq.0).and.(o.eq.ord)) &
             CALL entrans(C1,C2,C3,C7,C8,C4,ext,1)
          rmp = 1./real(o,kind=GP)
@@ -16,11 +16,11 @@
          DO j = 1,ny
          DO k = 1,nz
             IF ((kn2(k,j,i).le.kmax).and.(kn2(k,j,i).ge.tiny)) THEN
-               vx(k,j,i) = C1(k,j,i)+dt*(nu*vx(k,j,i)+C7(k,j,i) &
+               vx(k,j,i) = C1(k,j,i)+dt*(vx(k,j,i)+C7(k,j,i) &
               +fx(k,j,i))*rmp
-               vy(k,j,i) = C2(k,j,i)+dt*(nu*vy(k,j,i)+C8(k,j,i) &
+               vy(k,j,i) = C2(k,j,i)+dt*(vy(k,j,i)+C8(k,j,i) &
               +fy(k,j,i))*rmp
-               vz(k,j,i) = C3(k,j,i)+dt*(nu*vz(k,j,i)+C4(k,j,i) &
+               vz(k,j,i) = C3(k,j,i)+dt*(vz(k,j,i)+C4(k,j,i) &
               +fz(k,j,i))*rmp
             ELSE
                vx(k,j,i) = 0.0_GP
