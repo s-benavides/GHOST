fed2c26268f323a8273da69a7fa9ece928f53acc
Santiago Benavides <santib@mit.edu>
2019-07-08 17:36:38 -0400

    Fixed hyper/hypo stuff and added 'odir'.

--- 3D/src/include/mhd_rkstep2.f90
+++ 3D/src/include/mhd_rkstep2.f90
@@ -7,12 +7,12 @@
          CALL prodre3(vx,vy,vz,C10,C11,C12)
          CALL prodre3(C7,C8,C9,C13,C14,C15)
          IF ((trans.eq.1).and.(times.eq.0).and.(bench.eq.0).and.(o.eq.ord)) &
-            CALL entrans(C1,C2,C3,C13,C14,C15,ext,2)
+            CALL entrans(C1,C2,C3,C13,C14,C15,ext,2,odir)
          CALL nonlin3(C10,C11,C12,C13,C14,C15,C16,1)
          CALL nonlin3(C10,C11,C12,C13,C14,C15,C17,2)
          CALL nonlin3(C10,C11,C12,C13,C14,C15,C10,3)
          IF ((trans.eq.1).and.(times.eq.0).and.(bench.eq.0).and.(o.eq.ord)) &
-            CALL entrans(C7,C8,C9,C16,C17,C10,ext,4)
+            CALL entrans(C7,C8,C9,C16,C17,C10,ext,4,odir)
          CALL vector3(vx,vy,vz,C7,C8,C9,C11,C12,C13)
          CALL gauge3(C11,C12,C13,C7,1)
          CALL gauge3(C11,C12,C13,C8,2)
@@ -25,12 +25,12 @@
          CALL diss(az,az,hem,hom,mu,hmu)
          IF ((trans.eq.1).and.(times.eq.0).and.(bench.eq.0).and.(o.eq.ord)) &
             THEN
-            CALL entrans(C1,C2,C3,C16,C17,C10,ext,1)
-            CALL entrans(C4,C5,C6,C7,C8,C9,ext,0)
+            CALL entrans(C1,C2,C3,C16,C17,C10,ext,1,odir)
+            CALL entrans(C4,C5,C6,C7,C8,C9,ext,0,odir)
             CALL rotor3(C8,C9,C11,1)
             CALL rotor3(C7,C9,C12,2)
             CALL rotor3(C7,C8,C13,3)
-            CALL entrans(C1,C2,C3,C11,C12,C13,ext,3)
+            CALL entrans(C1,C2,C3,C11,C12,C13,ext,3,odir)
          ENDIF
 
          rmp = 1./real(o,kind=GP)
@@ -40,17 +40,17 @@
          DO j = 1,ny
          DO k = 1,nz
             IF ((kn2(k,j,i).le.kmax).and.(kn2(k,j,i).ge.tiny)) THEN
-               vx(k,j,i) = C1(k,j,i)+dt*(nu*vx(k,j,i)+C16(k,j,i) &
+               vx(k,j,i) = C1(k,j,i)+dt*(vx(k,j,i)+C16(k,j,i) &
               +fx(k,j,i))*rmp
-               vy(k,j,i) = C2(k,j,i)+dt*(nu*vy(k,j,i)+C17(k,j,i) &
+               vy(k,j,i) = C2(k,j,i)+dt*(vy(k,j,i)+C17(k,j,i) &
               +fy(k,j,i))*rmp
-               vz(k,j,i) = C3(k,j,i)+dt*(nu*vz(k,j,i)+C10(k,j,i) &
+               vz(k,j,i) = C3(k,j,i)+dt*(vz(k,j,i)+C10(k,j,i) &
               +fz(k,j,i))*rmp
-               ax(k,j,i) = C4(k,j,i)+dt*(mu*ax(k,j,i)+C7(k,j,i)  &
+               ax(k,j,i) = C4(k,j,i)+dt*(ax(k,j,i)+C7(k,j,i)  &
               +mx(k,j,i))*rmp
-               ay(k,j,i) = C5(k,j,i)+dt*(mu*ay(k,j,i)+C8(k,j,i)  &
+               ay(k,j,i) = C5(k,j,i)+dt*(ay(k,j,i)+C8(k,j,i)  &
               +my(k,j,i))*rmp
-               az(k,j,i) = C6(k,j,i)+dt*(mu*az(k,j,i)+C9(k,j,i)  &
+               az(k,j,i) = C6(k,j,i)+dt*(az(k,j,i)+C9(k,j,i)  &
               +mz(k,j,i))*rmp
             ELSE
                vx(k,j,i) = 0.0_GP
