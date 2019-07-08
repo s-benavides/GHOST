ed3941dab8dffa1d7e8746e50dfbd78eb29a6382
Santiago Benavides <santib@mit.edu>
2019-07-08 17:37:11 -0400

    Added 'odir'.

--- 3D/src/include/mhd_spectrum.f90
+++ 3D/src/include/mhd_spectrum.f90
@@ -1,5 +1,5 @@
 ! Spectra computed in MHD runs
 
-            CALL spectrum(vx,vy,vz,ext,1,1)
-            CALL spectrum(ax,ay,az,ext,0,1)
-            CALL crosspec(vx,vy,vz,ax,ay,az,ext)
+            CALL spectrum(vx,vy,vz,ext,1,1,odir)
+            CALL spectrum(ax,ay,az,ext,0,1,odir)
+            CALL crosspec(vx,vy,vz,ax,ay,az,ext,odir)
