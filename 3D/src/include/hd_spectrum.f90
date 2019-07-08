be2fb11bca75d6393fd6eb7619f2852e1eae7d45
Santiago Benavides <santib@mit.edu>
2019-07-08 17:18:03 -0400

    Corrected spectrum subroutine call to have 'odir' in it.

--- 3D/src/include/hd_spectrum.f90
+++ 3D/src/include/hd_spectrum.f90
@@ -1,14 +1,14 @@
 ! Spectra computed in HD runs
 
-            CALL spectrum(vx,vy,vz,ext,1,1)
+            CALL spectrum(vx,vy,vz,ext,1,1,odir)
 
 ! Uncomment the following lines to compute 1D spectra
-!           CALL spectr1d(vx,ext,'k',1,1)
-!           CALL spectr1d(vy,ext,'k',2,1)
-!           CALL spectr1d(vz,ext,'k',3,1)
-!           CALL spectr1d(vx,ext,'k',1,2)
-!           CALL spectr1d(vy,ext,'k',2,2)
-!           CALL spectr1d(vz,ext,'k',3,2)
-!           CALL spectr1d(vx,ext,'k',1,3)
-!           CALL spectr1d(vy,ext,'k',2,3)
-!           CALL spectr1d(vz,ext,'k',3,3)
+!           CALL spectr1d(vx,ext,'k',1,1,odir)
+!           CALL spectr1d(vy,ext,'k',2,1,odir)
+!           CALL spectr1d(vz,ext,'k',3,1,odir)
+!           CALL spectr1d(vx,ext,'k',1,2,odir)
+!           CALL spectr1d(vy,ext,'k',2,2,odir)
+!           CALL spectr1d(vz,ext,'k',3,2,odir)
+!           CALL spectr1d(vx,ext,'k',1,3,odir)
+!           CALL spectr1d(vy,ext,'k',2,3,odir)
+!           CALL spectr1d(vz,ext,'k',3,3,odir)
