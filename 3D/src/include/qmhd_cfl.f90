1bae24c6bcf0d664c3c89cbb7dfe2d7c0b0c53d0
Santiago J Benavides <49104173+s-benavides@users.noreply.github.com>
2021-01-13 17:12:54 -0500

    Fixed CFL condition for lorentz force

--- 3D/src/include/qmhd_cfl.f90
+++ 3D/src/include/qmhd_cfl.f90
@@ -1,7 +1,7 @@
         CALL maxabs(vx,vy,vz,rmp,0)  ! max vort = k U
         tmp = sqrt((omegax)**2+(omegay)**2+(omegaz)**2)
         kcut = real(nx,kind=GP)/3.0_GP    !1/dx        
-        tmq = kcut**2*(NNx**2+2*NNx*NNz+NNz**2) ! Lorentz force term
+        tmq = NNx**2+2*NNx*NNz+NNz**2 ! Lorentz force term
 
         dt = cfl/max(rmp,tmp,nu*kcut**(2*hek),hnu,tmq)
         CALL MPI_BCAST(dt,1,GC_REAL,0,MPI_COMM_WORLD,ierr)
