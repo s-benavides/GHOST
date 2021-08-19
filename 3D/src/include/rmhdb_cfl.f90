b4a21dd327af726e87b7e0ee5a400bcc3248f93f
Santiago Benavides <santib@mit.edu>
2021-08-19 13:15:07 -0400

    Updated CFL time-scale for Alfven waves, which now computes k_parallel * B_0, instead of the isotropic sqrt(kmax^2) B_0. This is helpful for anisotropic boxes and grids.

--- 3D/src/include/rmhdb_cfl.f90
+++ 3D/src/include/rmhdb_cfl.f90
@@ -1,10 +1,11 @@
         CALL maxabs(vx,vy,vz,rmp,0)  ! max vort = k U
         CALL maxabs(ax,ay,az,rmq,1)  ! max j = k B
         tmp = sqrt((omegax)**2+(omegay)**2+(omegaz)**2)
-        tmq = sqrt((bx0)**2+(by0)**2+(bz0)**2)
+        !tmq = sqrt((bx0)**2+(by0)**2+(bz0)**2)
+        tmq = max(bx0*real(nx,kind=GP)/Lx,by0*real(ny,kind=GP)/Ly,bz0*real(nz,kind=GP)/Lz)/3.0_GP !1/dx
         kcut = max(real(nx,kind=GP)/Lx,real(ny,kind=GP)/Ly,real(nz,kind=GP)/Lz)/3.0_GP !1/dx
 
-        dt = cfl/max(rmp,rmq,tmp,kcut*tmq,nu*kcut**(2*hek),&
+        dt = cfl/max(rmp,rmq,tmp,tmq,nu*kcut**(2*hek),& ! previously: kcut*tmq
                    mu*kcut**(2*hem),hnu,hmu)
         CALL MPI_BCAST(dt,1,GC_REAL,0,MPI_COMM_WORLD,ierr)
 
