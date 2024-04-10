2752cce07b800bf313eadb06889f287c25995acf
s-benavides <santiago.benavides@upm.es>
2024-04-10 14:18:29 +0200

    Now outputting KE, and both ME flux, apart from flux of A^2.

--- 2D/src/include/mhd_rkstep2.f90
+++ 2D/src/include/mhd_rkstep2.f90
@@ -1,17 +1,20 @@
 ! Step 2 of Runge-Kutta for the MHD equations
 ! Computes the nonlinear terms and evolves the equations in dt/o
-         CALL laplak2(ps,C3)
-         CALL laplak2(az,C4)
-         CALL poisson(ps,az,C5)
+         CALL laplak2(ps,C3) ! Vorticity -W = -k^2 * psi
+         CALL laplak2(az,C4) ! Current -J = -k^2 * a
+         CALL poisson(ps,az,C5) ! -curl( u x a ) = [psi,a]
          IF ((trans.eq.1).and.(times.eq.0).and.(bench.eq.0).and.(o.eq.ord)) &
-            CALL vectrans(az,C5,ext)
-         CALL poisson(ps,C3,ps)
-         CALL poisson(az,C4,az)
+            CALL vectrans(az,C5,ext) ! a [ psi, a ]
+         CALL poisson(ps,C3,ps) ! [psi, W]
+         CALL poisson(az,C4,az) ! [a, J]
+         IF ((trans.eq.1).and.(times.eq.0).and.(bench.eq.0).and.(o.eq.ord)) &
+            CALL entrans(C1,ps,ext) ! psi [psi,W]. uuu
+            CALL entrans_bbu(C1,az,ext) ! psi [a , J]. bbu
+            CALL entrans_bub(C4,C5,ext) ! -J [psi,a]. bub+ubb
 
          rmp = 1.0_GP/real(o,kind=GP)
          DO i = ista,iend
             DO j = 1,n
-            
             IF ((ka2(j,i).le.kmax).and.(ka2(j,i).ge.tiny)) THEN
                ps(j,i) = C1(j,i)+dt*((nu*ka2(j,i)**(hek-1) + hnu*ka2(j,i)**(-hok-1))*C3(j,i)+(az(j,i)-ps(j,i))    &
               /ka2(j,i)+fk(j,i))*rmp
